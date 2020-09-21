9488501b048ba86030d8edfc594d9980aa689bb2
czhcheng27 <czhcheng27@gmail.com>
2020-09-21 00:08:45 -0400

    done form validation

--- react-admin_client/src/pages/login/login.jsx
+++ react-admin_client/src/pages/login/login.jsx
@@ -10,6 +10,23 @@ export default class Login extends Component {
         console.log('Received values of form: ', values);
       };
 
+      validatorPwd = (rule, value) => {
+        //   console.log('validatorPwd', rule, value)
+          return new Promise(async (resolve, reject) => {
+            if (!value) {
+                await reject('Please input your Password!')
+            } else if (value.length < 4) {
+                await reject('Password should at least 4 digits!')
+            } else if (value.length > 12) {
+                await reject('Password can\'t longer than 12 digits!')
+            } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+                await reject('Password can only consist of letter, number and _!')
+            } else {
+                await resolve()
+            }
+        })
+      }
+
     render() {
         return (
             <div className='login'>
@@ -21,13 +38,28 @@ export default class Login extends Component {
 
                 <section className='login-content'>
                     <h1>User Login</h1>
+                    {
+                        /* 
+                        username
+                        1. must type in
+                        2. longer than 4 digits
+                        3. < 12
+                        4. consist of letter, number and _
+                        */
+                    }
                     <Form
                     className="login-form"
                     initialValues={{ remember: true }}
                     onFinish={this.onFinish}
                     >
                         <Form.Item
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                        name='username'
+                        rules={[
+                            { required: true, message: 'Please input your Username!' },
+                            { max: 12, message: 'Username can\'t longer than 12 digits!' },
+                            { min: 4, message: 'Username should at least 4 digits!' },
+                            { pattern: /^[a-zA-Z0-9_]+$/, message: 'Username can only consist of letter, number and _!' },
+                        ]}
                         >
                         <Input 
                         prefix={<UserOutlined className="site-form-item-icon" />} 
@@ -35,7 +67,8 @@ export default class Login extends Component {
                         </Form.Item>
 
                         <Form.Item
-                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                        name='password'
+                        rules={[{ validator: this.validatorPwd }]}
                         >
                         <Input.Password
                         className='password'
