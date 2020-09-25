c36fe63f36406e0a1f55636f89732eb6c2d40a92
czhcheng27 <czhcheng27@gmail.com>
2020-09-24 22:28:32 -0400

    header.jsx static component

--- react-admin_client/src/components/header/header.jsx
+++ react-admin_client/src/components/header/header.jsx
@@ -4,7 +4,16 @@ import './index.less'
 export default class Header extends Component { 
     render () { 
         return ( 
-            <div className='header'>Header</div>
+            <div className='header'>
+                <div className='header-top'>
+                    <span>Welcome, admin</span>
+                    <a href='javascript:'>Log Out</a>
+                </div>
+                <div className='header-bottom'>
+                    <div className='header-bottom-left'>Home</div>
+                    <div className='header-bottom-right'>2020-9-24</div>
+                </div>
+            </div>
         )
     }
 }
