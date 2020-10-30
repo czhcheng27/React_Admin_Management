ecdd97cb5c8f7a4708a8e8b11dade5e567145733
czhcheng27 <czhcheng27@gmail.com>
2020-10-29 22:43:33 -0400

    charts

--- react-admin_client/src/pages/charts/bar.jsx
+++ react-admin_client/src/pages/charts/bar.jsx
@@ -1,9 +1,54 @@
 import React, {Component} from 'react' 
+import {Button, Card} from 'antd'
+import ReactEcharts from 'echarts-for-react';
 
 export default class Bar extends Component { 
+    
+    state = {
+        sales:[5, 20, 36, 10, 10, 20],
+        stock:[15, 23, 33, 1, 15, 10]
+    }
+
+    getOption = (sales, stock) => {
+
+        return {
+            title: {
+                text: 'ECharts entry example'
+            },
+            tooltip: {},
+            legend: {
+                data:['Sales', 'Stock']
+            },
+            xAxis: {
+                data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
+            },
+            yAxis: {},
+            series: [{
+                name: 'Sales',
+                type: 'bar',
+                data: sales
+            },{
+                name: 'Stock',
+                type: 'bar',
+                data: stock
+            }]
+        };
+
+    }
+
     render () { 
+
+        const {sales, stock} = this.state
         return ( 
-            <div>Bar</div>
+            <div>
+                <Card>
+                    <Button type='primary'>Update</Button>
+                </Card>
+
+                <Card title="Bar Image">
+                    <ReactEcharts option={this.getOption(sales, stock)} />
+                </Card>
+            </div>
         )
     }
 }
--- react-admin_client/src/pages/charts/pie.jsx
+++ react-admin_client/src/pages/charts/pie.jsx
@@ -1,9 +1,132 @@
-import React, {Component} from 'react' 
+import React, { Component } from 'react'
+import { Button, Card } from 'antd'
+import ReactEcharts from 'echarts-for-react';
 
-export default class Pie extends Component { 
-    render () { 
-        return ( 
-            <div>Pie</div>
+export default class Pie extends Component {
+
+    getOption = () => {
+        return {
+            backgroundColor: '#2c343c',
+
+            title: {
+                text: 'Customized Pie',
+                left: 'center',
+                top: 20,
+                textStyle: {
+                    color: '#ccc'
+                }
+            },
+
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+
+            visualMap: {
+                show: false,
+                min: 80,
+                max: 600,
+                inRange: {
+                    colorLightness: [0, 1]
+                }
+            },
+            series: [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '50%'],
+                    data: [
+                        { value: 335, name: 'Direct Visit' },
+                        { value: 310, name: 'Email AD' },
+                        { value: 274, name: 'League AD' },
+                        { value: 235, name: 'Video AD' },
+                        { value: 400, name: 'Search Engine' }
+                    ].sort(function (a, b) { return a.value - b.value; }),
+                    roseType: 'radius',
+                    label: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    },
+                    labelLine: {
+                        lineStyle: {
+                            color: 'rgba(255, 255, 255, 0.3)'
+                        },
+                        smooth: 0.2,
+                        length: 10,
+                        length2: 20
+                    },
+                    itemStyle: {
+                        color: '#c23531',
+                        shadowBlur: 200,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    },
+
+                    animationType: 'scale',
+                    animationEasing: 'elasticOut',
+                    animationDelay: function (idx) {
+                        return Math.random() * 200;
+                    }
+                }
+            ]
+        }
+    }
+
+    getOption2 = () => {
+        return {
+            title: {
+                text: 'Source of user visits to a site',
+                subtext: 'Purely fictitious',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                data: ['Direct Visit', 'Email AD', 'League AD', 'Video AD', 'Search Engine']
+            },
+            series: [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '60%'],
+                    data: [
+                        {value: 335, name: 'Direct Visit'},
+                        {value: 310, name: 'Email AD'},
+                        {value: 234, name: 'League AD'},
+                        {value: 135, name: 'Video AD'},
+                        {value: 1548, name: 'Search Engine'}
+                    ],
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Card>
+                    <Button type='primary'>Update</Button>
+                </Card>
+
+                <Card title="Pie Image 1">
+                    <ReactEcharts option={this.getOption()} />
+                </Card>
+
+                <Card title="Pie Image 2">
+                    <ReactEcharts option={this.getOption2()} />
+                </Card>
+            </div>
         )
     }
 }
